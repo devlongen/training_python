b98050349851b44abc79b8990c8f038e8e585085
IAGO <iago.mendonca_2005@hotmail.com>
2023-11-25 11:38:50 -0300

    Corrigindo bug, e atualizando validacoes

--- Calculadora_imc.py
+++ Calculadora_imc.py
@@ -1,25 +1,26 @@
 loop_calculadora = True
 while loop_calculadora:
     nome_usuario = str(input("Escreva o seu nome > "))
-    try:
-        idade_usuario = int(input("Digite a sua idade > "))
-        if idade_usuario >= 0 and <= 100:
+        try:
+            idade_usuario = int(input("Digite a sua idade > "))
+            if idade_usuario >= 0 and idade_usuario <= 100:
+                break
+            else:
+                print("Erro: número inválido")
+        except ValueError as e:
+            peso_usuario = float(input("Digite o seu peso em kg > "))
+        if peso_usuario >= 0 and peso_usuario <= 500.00:
             break
         else:
             print("Erro: número inválido")
     except ValueError as e:
-        print(f"Erro: valor diferente de inteiro! {e}")
-    peso_usuario = float(input("Digite o seu peso em kg > "))
-    try:
-        peso_usuario = float(input("Digite o seu peso em kg > "))
-        if peso_usuario == 0 and 500.00:
+        altura_usuario = float(input("Digite a sua altura em metros > "))
+        if altura_usuario >= 0 and altura_usuario <= 2.20:
             break
         else:
             print("Erro: número inválido")
     except ValueError as e:
-        print(f"Erro: tipo de dado inválido {e}")
-    altura_usuario = float(input("Digite a sua altura em metros > "))
-    resposta_usuario = str(input("Vamos calcular o seu imc? (sim ou não)"))
+resposta_usuario = str(input("Vamos calcular o seu imc? (sim ou não)"))
     if resposta_usuario == 'sim':
             resultado_imc = peso_usuario / (altura_usuario *
             altura_usuario)
