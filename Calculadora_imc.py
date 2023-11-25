ecdaecb1229abd99a5dc467783e58a4ec09c3fcb
IAGO <iago.mendonca_2005@hotmail.com>
2023-11-25 08:51:30 -0300

    Fazendo validações

--- Calculadora_imc.py
+++ Calculadora_imc.py
@@ -1,8 +1,23 @@
 loop_calculadora = True
 while loop_calculadora:
     nome_usuario = str(input("Escreva o seu nome > "))
-    idade_usuario = int(input("Digite a sua idade > "))
+    try:
+        idade_usuario = int(input("Digite a sua idade > "))
+        if idade_usuario >= 0 and <= 100:
+            break
+        else:
+            print("Erro: número inválido")
+    except ValueError as e:
+        print(f"Erro: valor diferente de inteiro! {e}")
     peso_usuario = float(input("Digite o seu peso em kg > "))
+    try:
+        peso_usuario = float(input("Digite o seu peso em kg > "))
+        if peso_usuario == 0 and 500.00:
+            break
+        else:
+            print("Erro: número inválido")
+    except ValueError as e:
+        print(f"Erro: tipo de dado inválido {e}")
     altura_usuario = float(input("Digite a sua altura em metros > "))
     resposta_usuario = str(input("Vamos calcular o seu imc? (sim ou não)"))
     if resposta_usuario == 'sim':
