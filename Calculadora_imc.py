4d240a2a09c6f475707b5a44e35f879846d3a689
IAGO <iago.mendonca_2005@hotmail.com>
2023-11-25 00:44:52 -0300

    Calculadora IMC (projetando)

---
+++ Calculadora_imc.py
@@ -0,0 +1,19 @@
+loop_calculadora = True
+while loop_calculadora:
+    nome_usuario = str(input("Escreva o seu nome > "))
+    idade_usuario = int(input("Digite a sua idade > "))
+    peso_usuario = float(input("Digite o seu peso em kg > "))
+    altura_usuario = float(input("Digite a sua altura em metros > "))
+    resposta_usuario = str(input("Vamos calcular o seu imc? (sim ou não)"))
+    if resposta_usuario == 'sim':
+            resultado_imc = peso_usuario / (altura_usuario *
+            altura_usuario)
+            print("Seu calculo ficou entre o valor:",
+            resultado_imc)
+    else:
+         print("Tudo bem, tenha um bom dia!")
+    resposta_loop = str(input("Deseja continuar? s/n"))
+    loop_calculadora = resposta_loop.lower() == 's'
+
+
+
