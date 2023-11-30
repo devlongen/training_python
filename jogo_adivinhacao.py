1cba5e579a0189d168205ae245bbcdf39adcb781
Iago Longen <125667107+devlongen@users.noreply.github.com>
2023-11-30 11:36:43 -0300

    Criação do jogo de adivinhação, primeiros passos!

---
+++ jogo_adivinhacao.py
@@ -0,0 +1,8 @@
+loop_adivinha = True
+while loop_adivinha:
+    print("-=Seja bem vindo ao jogo de adivinhação em Python=-")
+    print("   Para começarmos preciso de suas informações  ")
+
+    nome_usuario = str(input("Diga o seu nome >"))
+    idade_usuario = int(input("Diga a sua idade >"))
+    
