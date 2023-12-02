4d240c1125d74ed611fbfb5046df9a24bb576aca
IAGO <iago.mendonca_2005@hotmail.com>
2023-12-01 22:58:20 -0300

    Criação da interface e validando o dado de entrada

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -2,7 +2,15 @@ loop_adivinha = True
 while loop_adivinha:
     print("-=Seja bem vindo ao jogo de adivinhação em Python=-")
     print("   Para começarmos preciso de suas informações  ")
-
     nome_usuario = str(input("Diga o seu nome >"))
     idade_usuario = int(input("Diga a sua idade >"))
-    
+    while True:
+        resposta_usuario = str(input("Vamos comecar a jogar? (sim ou nao)"))
+        if resposta_usuario == "sim":
+            break
+        elif resposta_usuario == "nao":
+            print("Espero que tenha um bom dia!")
+            loop_adivinha = False
+            break
+        else:
+            print("Escreveu algo invalido ou algo diferente de sim ou nao")
