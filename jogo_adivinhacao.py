505b5ce73e28da5ce7132643f6fb9c6fa78da2f1
IAGO <iago.mendonca_2005@hotmail.com>
2023-12-04 17:08:17 -0300

    Criado variaveis e randomizar o numero, pode ter alguns bugs!

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -1,4 +1,8 @@
+import random
+
 loop_adivinha = True
+tentativas = 3
+numero_aleatorio = random.randint(1,100)
 while loop_adivinha:
     print("-=Seja bem vindo ao jogo de adivinhação em Python=-")
     print("   Para começarmos preciso de suas informações  ")
@@ -14,3 +18,14 @@ while loop_adivinha:
             break
         else:
             print("Escreveu algo invalido ou algo diferente de sim ou nao")
+    while tentativas:
+        tentativa_jogador = int(input("Voce tera 3 tentativas de acertar um numero aleatorio de 1 ate 100, BOA SORTE!"))
+        if tentativa_jogador == numero_aleatorio:
+            print("Parabens voce acertou o numero!")
+        elif tentativa_jogador < numero_aleatorio:
+            print("Esta perto o numero e maior!")
+            tentativas = tentativas - 1
+        else:
+            print("Esta perto o numero e menor!")
+            tentativas = tentativas - 1
+    
