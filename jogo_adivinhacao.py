4d878425b7c596c5a269f7a47e6b20e1f9ead3fb
Iago Longen <125667107+devlongen@users.noreply.github.com>
2023-12-06 10:28:52 -0300

    Jogo funcionando mas falta criar validações gerais

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -7,26 +7,33 @@ while loop_adivinha:
     print("-=Seja bem vindo ao jogo de adivinhação em Python=-")
     print("   Para começarmos preciso de suas informações  ")
     nome_usuario = str(input("Diga o seu nome >"))
-    idade_usuario = int(input("Diga a sua idade >"))
     while True:
-        resposta_usuario = str(input("Vamos comecar a jogar? (sim ou nao)"))
+        idade_usuario = int(input("Diga a sua idade >"))
+        if 0 <= idade_usuario <= 99:
+            break
+        else:
+            print("Idade inválida você inseriu um valor não congruente!")
+    while True:
+        resposta_usuario = str(input("Vamos comecar a jogar? (sim ou não)"))
         if resposta_usuario == "sim":
             break
-        elif resposta_usuario == "nao":
+        elif resposta_usuario == "não":
             print("Espero que tenha um bom dia!")
             loop_adivinha = False
             break
         else:
-            print("Escreveu algo invalido ou algo diferente de sim ou nao")
+            print("Escreveu algo inválido ou algo diferente de sim ou não")
     while tentativas:
-        tentativa_jogador = int(input("Voce tera 3 tentativas de acertar um numero aleatorio de 1 ate 100, BOA SORTE!"))
+        tentativa_jogador = int(input("Você terá 3 tentativas de acertar um número aleatório de 1 até 100, BOA SORTE!"))
         if tentativa_jogador == numero_aleatorio:
-            print("Parabens voce acertou o numero!")
+            print("Parabéns! Você acertou o número!")
             loop_adivinha = False
         elif tentativa_jogador < numero_aleatorio:
-            print("Esta perto o numero e maior!")
-            tentativas = tentativas - 1
+            print("Você está perto, mas o número é maior!")
         else:
-            print("Esta perto o numero e menor!")
-            tentativas = tentativas - 1
+            print("Você está perto, mas o número é menor!")
+        tentativas -= 1
+        if tentativas == 0:
+            print(f"Fim das tentativas. O número era {numero_aleatorio}.")
+            loop_adivinha = False
     
