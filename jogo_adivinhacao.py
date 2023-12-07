7f0e6730ece264272ba80652161dd54b4ed45b56
Iago Longen <125667107+devlongen@users.noreply.github.com>
2023-12-07 10:54:21 -0300

    Comentários e validando, falta testar o código

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -1,8 +1,11 @@
 import random
-
+#biblioteca de randomizar numero
 loop_adivinha = True
 tentativas = 3
 numero_aleatorio = random.randint(1,100)
+numero_proximo = numero_aleatorio
+# declaração das variáveis
+# código do jogo de adivinhação
 while loop_adivinha:
     print("-=Seja bem vindo ao jogo de adivinhação em Python=-")
     print("   Para começarmos preciso de suas informações  ")
@@ -13,6 +16,7 @@ while loop_adivinha:
             break
         else:
             print("Idade inválida você inseriu um valor não congruente!")
+# pedindo e validando as informações pessoais do usuário
     while True:
         resposta_usuario = str(input("Vamos comecar a jogar? (sim ou não)"))
         if resposta_usuario == "sim":
@@ -23,17 +27,30 @@ while loop_adivinha:
             break
         else:
             print("Escreveu algo inválido ou algo diferente de sim ou não")
+# pedindo e validando as informações para jogar do usuário
     while tentativas:
         tentativa_jogador = int(input("Você terá 3 tentativas de acertar um número aleatório de 1 até 100, BOA SORTE!"))
         if tentativa_jogador == numero_aleatorio:
             print("Parabéns! Você acertou o número!")
             loop_adivinha = False
         elif tentativa_jogador < numero_aleatorio:
-            print("Você está perto, mas o número é maior!")
+            numero_proximo = numero_aleatorio + 5
+            print("Você está perto, mas o número é maior e próximo de:", numero_proximo)
         else:
-            print("Você está perto, mas o número é menor!")
+            numero_proximo = numero_aleatorio - 5
+            print("Você está perto, mas o número é menor e próximo de:", numero_proximo)
         tentativas -= 1
         if tentativas == 0:
             print(f"Fim das tentativas. O número era {numero_aleatorio}.")
             loop_adivinha = False
-    
+# funcionamento do jogo e as tentativas
+    while True:
+        resposta_recomecar = str(input("Você deseja tentar novamente? (sim ou não)"))
+        if resposta_recomecar == "sim":
+            break
+        elif resposta_recomecar == "não":
+            print("Espero que tenha gostado do jogo!")
+            loop_adivinha = False
+        else:
+            print("Você digitou algo inválido ou que não condiz com a pergunta, tente novamente!")
+# perguntando para o usuário caso deseja tentar novamente
