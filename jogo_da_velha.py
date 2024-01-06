ff68ab85000a1cd94f2591b55087038aef381be7
IAGO <iago.mendonca_2005@hotmail.com>
2024-01-06 14:30:44 -0300

    jogo da velha em python

---
+++ jogo_da_velha.py
@@ -0,0 +1,10 @@
+"""
+
+    - O tabuleiro é uma grade 3x3.
+    - Dois jogadores, 'X' e 'O', jogam alternadamente.
+    - Cada jogador pode escolher uma célula vazia e colocar seu marcador ('X' ou 'O') nela.
+    - O jogo continua até que um dos jogadores vença ou o tabuleiro esteja cheio (empate).
+    - Um jogador vence se conseguir três de seus marcadores em linha (horizontal, vertical ou diagonal).
+
+"""
+
